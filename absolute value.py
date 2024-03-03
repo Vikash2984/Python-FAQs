24a4362cade4585838e52c6e603060ee4c9271cf
Vikash Kumar Pandey <112485773+Vikash2984@users.noreply.github.com>
2024-03-03 10:54:21 +0530

    Delete absolute value.py

--- absolute value.py
+++
@@ -1,8 +0,0 @@
-def execmain():
-    x=int(input("\nEnter a number : "))
-    if(abs(x)==x):
-        print("You entered a postive no")
-    else:
-        x*=-1
-        print("Number made positive",x)
-execmain()
